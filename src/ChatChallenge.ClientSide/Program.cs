4e46e68aa460b8f7f3851b41c0d8d6cfa69264d6
Rafael Lima Marçal <rafaell.lima92@gmail.com>
2021-08-04 19:26:29 -0400

    Add project files.

---
+++ src/ChatChallenge.ClientSide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatChallenge.ClientSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ChatChallenge.Resources/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChatChallenge.Resources
+{
+    public class Class1
+    {
+    }
+}
